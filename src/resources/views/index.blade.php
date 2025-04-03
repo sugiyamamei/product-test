cfed9d7b20361ebd1d5835c6f28f157d50d04b62
sugiyamamei <sugiyamamei698947@gmail.com>
2025-04-04 01:27:23 +0900

    確認テスト２回目提出

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+
+<div class="product__content">
+    <h2>商品一覧</h2>
+        <div class="product__content_button">検索</div>
+</div>
+@endsection
